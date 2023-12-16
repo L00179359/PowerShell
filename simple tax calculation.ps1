8803456ac2c0dea7324a1e02ce9d8179f3eac78a
rahulkharade <rahul.ibmaix@gmail.com>
2023-12-16 19:51:37 +0000

    commit for simple tax calculation.ps1

---
+++ simple tax calculation.ps1
@@ -0,0 +1,7 @@
+﻿# calculate simple tax
+$NET = 10000
+$VAT = 0.23
+$VATAMOUNT = $NET * $VAT
+$GROSS = $NET - $VATAMOUNT
+$text = "The total €$GROSS is the sum of the net value €$amount with the VAT amount €$VATAMOUNT at $VAT% VAT rate"
+$text
