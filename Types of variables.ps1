55bc78db475d19521476bbf26f079d4d7b14e095
rahulkharade <rahul.ibmaix@gmail.com>
2023-12-16 19:58:46 +0000

    commit for variables

---
+++ Types of variables.ps1
@@ -0,0 +1,21 @@
+﻿# Characters and strings have the usual functions available, review the options at the reference provided.
+$StringValue = "Cloud Computing"
+$StringValue.ToUpper()
+$StringValue.ToLower()
+
+# concept of an array
+$MyArray = 1,2,3,4,5
+$MyArray[1]
+
+#Rather than a single Integer type, we have int for 32-bit numbers and long for 64-bit numbers
+$LittleNumber = 12345
+$LittleNumber.GetType()
+$BigNumber = 123456789123456789
+$BigNumber.GetType()
+
+#Use floating point
+
+[float]$Floaty32 = 12.12
+$Floaty32.GetType()
+[double]$Floaty64 = 12345.1234
+$Floaty64.GetType()
---
+++ variables.ps1
@@ -0,0 +1,16 @@
+﻿# create variable Atu and assign value for Atu variable
+$Atu = 1, 2, "a", "££"
+# show values assigned to variable Atu
+$Atu
+# clear vallues assigned to variable Atu
+clear-variable -Name Atu
+$Atu
+# Remove variable Atu
+Remove-Variable -Name Atu
+
+
+# Declare variable Cloud and insert values
+$Cloud = 1, 2, "a", "££"
+
+# show type of variable
+$Cloud.GetType()
